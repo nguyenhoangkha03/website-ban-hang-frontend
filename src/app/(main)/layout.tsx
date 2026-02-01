52ec38e1968c2de8059e81b0d7924a66a8cdb23a
toikun <hovtoi@gmail.com>
2026-02-01 15:39:32 +0700

    Ẩn chức năng đăng nhập, fix giao diện header

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,5 +1,5 @@
 // src/app/(main)/layout.tsx
-import Header from "@/components/layout/Header"; 
+import Header from "@/components/layout/Header-Login"; 
 import Footer from "@/components/layout/Footer"; 
 import MissingInfoBanner from "@/components/layout/MissingInfoBanner";
 
@@ -7,7 +7,9 @@ export default function MainLayout({ children }: { children: React.ReactNode }) 
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <MissingInfoBanner /> 
+
+      {/* Ẩn chức năng đăng nhập */}
+      {/* <MissingInfoBanner />  */}
       
       <main className="flex-grow"> 
         {children}
